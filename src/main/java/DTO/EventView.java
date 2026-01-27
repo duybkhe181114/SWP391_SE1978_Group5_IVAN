726b6c67bc88231b79ce337751673fada4c94f5a
duybkhe181114 <duybkhe181114@fpt.edu.vn>
2026-01-27 15:03:31 +0700

    Home Screen base + initialized library and database connection

---
+++ src/main/java/DTO/EventView.java
@@ -0,0 +1,81 @@
+package DTO;
+
+import java.time.LocalDateTime;
+
+public class EventView {
+    private Integer eventId;
+    private String eventName;
+    private String location;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    private Integer organizationId;
+    private String organizationName;
+    private String organizationLogoUrl;
+
+    // --- Getters and Setters ---
+
+    public Integer getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Integer eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public Integer getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(Integer organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+
+    public String getOrganizationLogoUrl() {
+        return organizationLogoUrl;
+    }
+
+    public void setOrganizationLogoUrl(String organizationLogoUrl) {
+        this.organizationLogoUrl = organizationLogoUrl;
+    }
+}
