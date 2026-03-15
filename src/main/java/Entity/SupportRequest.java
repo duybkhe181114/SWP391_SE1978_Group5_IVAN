741edf5654bc225e885d67fbb2abb008970b9638
Nguyen Tung Giang <tunggiang0220@gmail.com>
2026-03-16 00:12:47 +0700

    chinh lai ten thuoc tinh

--- src/main/java/Entity/SupportRequest.java
+++ src/main/java/Entity/SupportRequest.java
@@ -42,7 +42,7 @@ public class SupportRequest {
     }
 
     public SupportRequest(Integer requestId, String title, String description,
-                                 String supportType, String priority,
+                                 String categoryId, String priority,
                                  String supportLocation, String beneficiaryName,
                                  Integer affectedPeople, Double estimatedAmount,
                                  String contactEmail, String contactPhone,
